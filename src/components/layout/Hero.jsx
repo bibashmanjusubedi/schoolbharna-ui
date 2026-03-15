97cafc92405bbb6246ca05ec164203a00475d6a1
bibashmanjusubedi <acharabibash@outlook.com>
2026-03-15 12:57:31 +0545

    fix: Input border line removed

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -25,7 +25,7 @@ export default function FindTheRightCourse(){
     </svg></span>
                     </div>
                    
-                    <svg className="find-course-divider" width="1" height="49" viewBox="0 0 1 49" fill="black" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="find-course-divider" viewBox="0 0 1 49" fill="black" xmlns="http://www.w3.org/2000/svg">
 <line x1="0.5" x2="0.5" y2="49" stroke="#CECECE"/>
 </svg>
 
