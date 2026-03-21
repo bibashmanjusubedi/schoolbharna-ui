4cac7ee2f71000fa7fbee5bd61f5834573d62370
bibashmanjusubedi <acharabibash@outlook.com>
2026-03-21 16:15:16 +0545

    feat: Course Help Section jsx

---
+++ src/components/layout/CourseHelp.jsx
@@ -0,0 +1,20 @@
+import "./CourseHelp.css";
+
+export default function CourseHelp(){
+    return (
+        <section className="course-help">
+            <div className= "course-help-container">
+                
+                <div className="course-help-header">
+                    <h2>In doubt about which course to take?</h2>
+                    <p>
+                        See information on course length, area of activity, job market,cutt-off points, average salary and more.
+                    </p>
+                </div>
+
+                <a href="/" className="view-all">View all courses</a>
+                
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/layout/Navbar";
 import FindTheRightCourse from "../components/layout/Hero";
 import HomeCards from "../components/layout/HomeCards";
+import CourseHelp from "../components/layout/CourseHelp";
 import "./Home.css";
 
 export default function Home(){
@@ -9,6 +10,7 @@ export default function Home(){
             <Navbar/>
             <FindTheRightCourse/>
             <HomeCards />
+            <CourseHelp />
         </>
     )
 }
