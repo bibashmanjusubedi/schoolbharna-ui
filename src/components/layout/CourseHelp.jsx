098aa21ab1226cc8d4e22eaf6bde78cc7bbe9d8f
bibashmanjusubedi <acharabibash@outlook.com>
2026-03-22 13:58:40 +0545

    fix: Which Course to Take Section CSS fixed

--- src/components/layout/CourseHelp.jsx
+++ src/components/layout/CourseHelp.jsx
@@ -7,12 +7,20 @@ export default function CourseHelp(){
                 
                 <div className="course-help-header">
                     <h2>In doubt about which course to take?</h2>
-                    <p>
-                        See information on course length, area of activity, job market,cutt-off points, average salary and more.
-                    </p>
+
+                    <div className="course-help-subrow">
+                        <p>
+                            See information on course length, area of activity, job market,cutt-off points, average salary and more.
+                        </p>
+
+                        <a href="/" className="view-all">View all courses</a>
+
+                    </div>
+
+                    
                 </div>
 
-                <a href="/" className="view-all">View all courses</a>
+                
                 
             </div>
         </section>
