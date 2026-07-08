3d632e8d67fc021536519b77d37bbe29d6cbf1a2
bibashmanjusubedi <acharyabibash1@gmail.com>
2026-07-09 01:44:38 +0545

    Apple Store button

--- src/components/layout/CourseAppBanner.jsx
+++ src/components/layout/CourseAppBanner.jsx
@@ -2,7 +2,7 @@ import "./CourseAppBanner.css";
 
 import mobile from "../../assets/images/smartPhone2.png";
 import appStore from "../../assets/images/appStore.png";
-import googlePlay from "../../assets/images/googlePlayStore.png";
+import googlePlay from "../../assets/images/GooglePlayLogo.png";
 
 const CourseAppBanner = () => {
   return (
@@ -19,35 +19,52 @@ const CourseAppBanner = () => {
           <h2>Get matched to nucas courses</h2>
 
           <p>
-                With the nucas app it's never been easier to find your future.
-                Whether you can't decide on what to study or you're struggling to
-                pick a perfect location, our app can help!
+            With the nucas app it's never been easier to find your future.
+            Whether you can't decide on what to study or you're struggling to
+            pick a perfect location, our app can help!
           </p>
 
           <div className="banner-buttons">
-            <button className="learn-btn">
-                Find Out More
-            </button>
+            <button className="learn-btn">Find Out More</button>
 
-            <img
+            {/* <img
                 src={appStore}
                 alt="Download on App Store"
                 className="store-btn"
-            />
-           
-            <img
+            /> */}
+
+            <button className="app-store-btn">
+              <img
+                src={appStore}
+                alt="Download on App Store"
+                className="apple-icon"
+              />
+
+              <div className="app-store-text">
+                <span className="small-text">Download on the</span>
+                <span className="big-text">App Store</span>
+              </div>
+            </button>
+
+            {/* <button className="google-play-btn">
+              <img src={googlePlay} alt="Google Play" className="google-icon" />
+
+              <div className="store-text">
+                <span className="small-text">GET IT ON</span>
+                <span className="big-text">Google Play</span>
+              </div>
+            </button> */}
+
+            {/* <img
                 src={googlePlay}
                 alt="Get it on Google Play"
                 className="store-btn"
-            />
-
+            /> */}
           </div>
         </div>
-
       </div>
     </section>
   );
 };
-
 
 export default CourseAppBanner;
