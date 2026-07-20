a8e3982ccbc0d7d55393d47580b216fa4a3a264f
bibashmanjusubedi <acharyabibash1@gmail.com>
2026-07-20 19:26:40 +0545

    feat:CoursesDropDown with initial CSS

---
+++ src/components/layout/CoursesDropDown.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./CoursesDropDown.css";
+
+export default function CoursesDropDown({ closeDropDown }){
+    return (
+        <div className="dropdown-panel">
+            {/*Left Side: HighLight/Intro */ }
+            <div>
+                <div>
+                    {/* A Clean SVG resembling my image icon */}
+                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3f86d9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <line x1="8" y1="6" x2="21" y2="6"></line>
+                            <line x1="8" y1="12" x2="21" y2="12"></line>
+                            <line x1="8" y1="18" x2="21" y2="18"></line>
+                            <line x1="3" y1="6" x2="3.01" y2="6"></line>
+                            <line x1="3" y1="12" x2="3.01" y2="12"></line>
+                            <line x1="3" y1="18" x2="3.01" y2="18"></line>
+                    </svg> 
+                </div>
+                <h3>Courses &gt;</h3>
+                <p>Search, shortlist and compare thousands of courses to find the perfect one.</p>
+
+                {/* Middle Section: Popular Course Links columns */}
+                <div>
+                    <h4>Popular Courses</h4>
+                    <div>
+                        <div>
+                            <a href="/courses/accounting" onClick={closeDropDown}>Accounting</a>
+                            <a href="/courses/architecture" onClick={closeDropDown}>Architecture</a>
+                            <a href="/courses/civil" onClick={closeDropDown}>Civil Engineering</a>
+                            <a href="/courses/graphics" onClick={closeDropDown}>Graphic Design</a>
+                            <a href="/courses/law" onClick={closeDropDown}>Law</a>
+                            <a href="/courses/nursing" onClick={closeDropDown}>Nursing</a>
+                            <a href="/courses/psychology" onClick={closeDropDown}>Psychology</a>
+                        </div>
+                        <div>
+                            <a href="/courses/a-levels" onClick={closeDropDown}>A-Levels</a>
+                            <a href="/courses/bba" onClick={closeDropDown}>Business Administration</a>
+                            <a href="/courses/cs" onClick={closeDropDown}>Computer Science</a>
+                            <a href="/courses/dentistry" onClick={closeDropDown}>Dentistry</a>
+                            <a href="/courses/medicine" onClick={closeDropDown}>Medicine</a>
+                            <a href="/courses/pharmacy" onClick={closeDropDown}>Pharmacy</a>
+                        </div>
+                    </div>
+                    <a href="/courses" onClick={closeDropDown}>
+                        Browse all Courses <span>&gt;</span>
+                    </a>
+                </div>
+                {/* Right Section:Careers column */}
+                <div>
+                    <h4>Top Careers</h4>
+                    <div>
+                        <a href="/careers/accountant" onClick={closeDropDown}>How to become an Accountant</a>
+                        <a href="/careers/lawyer" onClick={closeDropDown}>How to become a Lawyer</a>
+                        <a href="/careers/psychologist" onClick={closeDropDown}>How to become a Psychologist</a>
+                    </div>
+                    <a href="/careers" className="blue-action-btn" onClick={closeDropDown}>
+                        Browse Top Careers <span>&gt;</span>
+                    </a>
+                </div>
+
+                
+            </div>
+        </div>
+    );
+}
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/images/logo.png";
 import loginIcon from "../../assets/images/loginIcon.png";
+import CoursesDropDown from "./CoursesDropDown";
 
 export default function Navbar(){
+    const [isCoursesOpen, setIsCoursesOpen] = useState(false);
+
+    const toggleCoursesDropDown = (e) => {
+        e.preventDefault();
+        setIsCoursesOpen(!isCoursesOpen);
+    };
+
     return(
         <header className="navbar">
             <div className="navbar-container">
@@ -13,8 +22,20 @@ export default function Navbar(){
                 </div>
 
                 <nav className="navbar-menu">
-                    <a href="/">Courses</a>
-                    <a href="/">Institutes</a>
+                    {/* Added active class and click event */}
+                    <div className="nav-item-container">
+                        <a href="/" onClick={toggleCoursesDropDown} className={`nav-link ${isCoursesOpen ? "active":""}`}>Courses<svg width="11" height="7" viewBox="0 0 11 7" fill="#464646" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.77277 5.82317L0.216514 1.26678C-0.0734701 0.976921 -0.0734701 0.506996 0.216514 0.2173C0.506238 -0.0724335 0.976142 -0.0724335 1.26584 0.2173L5.29744 4.249L9.3289 0.2174C9.61874 -0.072319 10.0886 -0.072319 10.3783 0.2174C10.6682 0.507133 10.6682 0.977043 10.3783 1.26688L5.82199 5.82328C5.67706 5.96815 5.48731 6.04049 5.29746 6.04049C5.10753 6.04049 4.91764 5.96802 4.77277 5.82317Z" fill="#464646"/>
+                        </svg></a>
+                        {/* Conditionally render the dropdown panel */}
+                        {isCoursesOpen && <CoursesDropDown closeDropdown={() => setIsCoursesOpen(false)} />}
+                    </div>
+                    
+                    <a href="/">Institutes <svg width="11" height="7" viewBox="0 0 11 7" fill="#464646" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.77277 5.82317L0.216514 1.26678C-0.0734701 0.976921 -0.0734701 0.506996 0.216514 0.2173C0.506238 -0.0724335 0.976142 -0.0724335 1.26584 0.2173L5.29744 4.249L9.3289 0.2174C9.61874 -0.072319 10.0886 -0.072319 10.3783 0.2174C10.6682 0.507133 10.6682 0.977043 10.3783 1.26688L5.82199 5.82328C5.67706 5.96815 5.48731 6.04049 5.29746 6.04049C5.10753 6.04049 4.91764 5.96802 4.77277 5.82317Z" fill="#464646"/>
+    </svg></a>
+  
+
                     <a href="/">Scholarship</a>
                     <a href="/">Exams</a>
                     <a href="/">Abouts</a>
